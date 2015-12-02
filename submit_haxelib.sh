e81570e662267c7b685c56b0bb5602b62920478d
Jeff Ward <jeff.ward@wootmath.com>
2015-12-02 09:07:23 -0700

    Added script to submit haxelib

---
+++ submit_haxelib.sh
@@ -0,0 +1,14 @@
+echo "Be sure to check version number in haxelib.json:"
+cat haxelib.json | grep -i version
+echo "lib.haxe.org currently has:"
+curl -s http://lib.haxe.org/p/hxtelemetry | grep 'Current version'
+sleep 1
+read -r -p "Are you sure? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+  rm -f hxtelemetry.zip
+  zip -r hxtelemetry.zip amf hxtelemetry haxelib.json include.xml README.md LICENSE
+  haxelib submit hxtelemetry.zip
+else
+  echo "Cancelled"
+fi
