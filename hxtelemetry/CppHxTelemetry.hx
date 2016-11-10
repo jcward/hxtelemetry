3c31bb2c1ba7fcd98ba1eebaa6b647ecb7eb8b64
Jeff Ward <jeff.ward@wootmath.com>
2016-11-10 08:21:39 -0700

    Hmm return null() works but was previously removed in e218aa6d... I suppose we'll just use an if clause as suggested by zeeshahmad Should fix #46, with hxcpp 3.3.49 and Haxe 3.2.1

--- hxtelemetry/CppHxTelemetry.hx
+++ hxtelemetry/CppHxTelemetry.hx
@@ -90,96 +90,97 @@ TelemetryFrame* frame = __hxcpp_hxt_dump_telemetry(thread_num);
 //         frame->allocations->size(),
 //   			frame->collections->size());
 
-if (frame==0 || output==null()) return;
+if (!(frame==0 || output==null())) {
 
-int i=0;
-int size;
+  int i=0;
+  int size;
 
-// Buffer for larger of samples/allocs
-::haxe::io::Bytes bytes;
-if (frame->samples!=0) { size = frame->samples->size(); }
-if (frame->allocation_data!=0) {
-  if (size<frame->allocation_data->size()) size = frame->allocation_data->size();
-}
-bytes = ::haxe::io::Bytes_obj::alloc((int)size*4);
+  // Buffer for larger of samples/allocs
+  ::haxe::io::Bytes bytes;
+  if (frame->samples!=0) { size = frame->samples->size(); }
+  if (frame->allocation_data!=0) {
+    if (size<frame->allocation_data->size()) size = frame->allocation_data->size();
+  }
+  bytes = ::haxe::io::Bytes_obj::alloc((int)size*4);
 
-if (frame->samples!=0) {
+  if (frame->samples!=0) {
 
-  // Write names
-  if (frame->names->size()>0) {
-    output->writeByte(10);
-    output->writeInt32(frame->names->size());
-    i = 0;
-    size = frame->names->size();
-    while (i<size) {
-      String s = String(frame->names->at(i++));
-      output->writeInt32(s.length);
-      output->writeString(s);
+    // Write names
+    if (frame->names->size()>0) {
+      output->writeByte(10);
+      output->writeInt32(frame->names->size());
+      i = 0;
+      size = frame->names->size();
+      while (i<size) {
+        String s = String(frame->names->at(i++));
+        output->writeInt32(s.length);
+        output->writeString(s);
+      }
+    }
+
+    // Write samples
+    if (frame->samples->size()>0) {
+      output->writeByte(11);
+      output->writeInt32(frame->samples->size());
+      i = 0;
+      size = frame->samples->size();
+      while (i<size) {
+        //output->writeInt32(frame->samples->at(i++));
+    		int d = frame->samples->at(i);
+    		bytes->b[(int)i*4]	 = d>>24;
+    		bytes->b[(int)i*4+1] = d>>16;
+    		bytes->b[(int)i*4+2] = d>>8;
+    		bytes->b[(int)i*4+3] = d;
+    		i++;
+    	}
+    	output->writeFullBytes(bytes, 0, size*4);
+    }
+
+  }
+
+  if (frame->allocation_data!=0) {
+
+    // Write stacks
+    if (frame->stacks->size()>0) {
+      output->writeByte(12);
+      output->writeInt32(frame->stacks->size());
+      i = 0;
+      size = frame->stacks->size();
+      while (i<size) {
+    		output->writeInt32(frame->stacks->at(i++));
+    	}
+    }
+
+    // Write allocations
+    if (frame->allocation_data->size()>0) {
+      // printf(\" -- writing allocs: %d\\n\", frame->allocation_data->size());
+      output->writeByte(13);
+      output->writeInt32(frame->allocation_data->size());
+      i = 0;
+      size = frame->allocation_data->size();
+
+      if (size>0) {
+        while (i<size) {
+        //  output->writeInt32(frame->allocation_data->at(i++));
+          int d = frame->allocation_data->at(i);
+    			bytes->b[(int)i*4]   = d>>24;
+    			bytes->b[(int)i*4+1] = d>>16;
+    			bytes->b[(int)i*4+2] = d>>8;
+    			bytes->b[(int)i*4+3] = d;
+          i++;
+        }
+        output->writeFullBytes(bytes, 0, size*4);
+    	}
     }
   }
 
-  // Write samples
-  if (frame->samples->size()>0) {
-    output->writeByte(11);
-    output->writeInt32(frame->samples->size());
-    i = 0;
-    size = frame->samples->size();
-    while (i<size) {
-      //output->writeInt32(frame->samples->at(i++));
-			int d = frame->samples->at(i);
-			bytes->b[(int)i*4]	 = d>>24;
-			bytes->b[(int)i*4+1] = d>>16;
-			bytes->b[(int)i*4+2] = d>>8;
-			bytes->b[(int)i*4+3] = d;
-			i++;
-		}
-		output->writeFullBytes(bytes, 0, size*4);
-  }
-
+  // GC time
+  hx::Anon gct = hx::Anon_obj::Create();
+  gct->Add(HX_CSTRING("name") , HX_CSTRING(".gc"),false);
+  gct->Add(HX_CSTRING("delta") , (int)frame->gctime,false);
+  gct->Add(HX_CSTRING("span") , (int)frame->gctime,false);
+  write_object(gct);
 }
-
-if (frame->allocation_data!=0) {
-
-  // Write stacks
-  if (frame->stacks->size()>0) {
-    output->writeByte(12);
-    output->writeInt32(frame->stacks->size());
-    i = 0;
-    size = frame->stacks->size();
-    while (i<size) {
-			output->writeInt32(frame->stacks->at(i++));
-		}
-  }
-
-  // Write allocations
-  if (frame->allocation_data->size()>0) {
-    // printf(\" -- writing allocs: %d\\n\", frame->allocation_data->size());
-    output->writeByte(13);
-    output->writeInt32(frame->allocation_data->size());
-    i = 0;
-    size = frame->allocation_data->size();
-
-    if (size>0) {
-      while (i<size) {
-      //  output->writeInt32(frame->allocation_data->at(i++));
-        int d = frame->allocation_data->at(i);
-  			bytes->b[(int)i*4]   = d>>24;
-				bytes->b[(int)i*4+1] = d>>16;
-				bytes->b[(int)i*4+2] = d>>8;
-				bytes->b[(int)i*4+3] = d;
-        i++;
-      }
-      output->writeFullBytes(bytes, 0, size*4);
-		}
-  }
-}
-
-// GC time
-hx::Anon gct = hx::Anon_obj::Create();
-gct->Add(HX_CSTRING("name") , HX_CSTRING(".gc"),false);
-gct->Add(HX_CSTRING("delta") , (int)frame->gctime,false);
-gct->Add(HX_CSTRING("span") , (int)frame->gctime,false);
-write_object(gct);
 
 ')
 
