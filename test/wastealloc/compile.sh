adef28a062441740c07df7b8d0b39f116c3c14e5
Jeff Ward <jeff.ward@wootmath.com>
2018-10-24 11:46:57 -0600

    Updated haxelib to 0.4.0

--- test/wastealloc/compile.sh
+++ test/wastealloc/compile.sh
@@ -1,1 +1,1 @@
-rm export/Main-debug; haxe Main.hx -main Main -debug -D HXCPP_STACK_TRACE -D HXCPP_TELEMETRY -cpp export -cp ../.. && ./export/Main-debug
+rm -f export/Main-debug; haxe Main.hx -main Main -debug -D HXCPP_STACK_TRACE -D HXCPP_TELEMETRY -cpp export -cp ../.. && ./export/Main-debug
