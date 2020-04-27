68e0da7719d7bf51b8b93e0a3bfef82b315f78bf
Jeff Ward <jeff.ward@gmail.com>
2020-04-27 14:03:37 -0600

    Fixed submit_haxelib.sh current version detection

--- submit_haxelib.sh
+++ submit_haxelib.sh
@@ -1,7 +1,7 @@
 echo "Be sure to check version number in haxelib.json:"
 cat haxelib.json | grep -i version
 echo "lib.haxe.org currently has:"
-curl -s http://lib.haxe.org/p/hxtelemetry | grep 'Current version'
+curl -s https://lib.haxe.org/p/hxtelemetry | grep '<title'
 sleep 1
 read -r -p "Are you sure? [y/N] " response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
